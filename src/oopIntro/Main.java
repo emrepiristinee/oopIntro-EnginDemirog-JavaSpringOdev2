e241e7ba8442024cda7288a59689cc1368f14322
Emre Piriştine <59123153+emrepiristinee@users.noreply.github.com>
2022-08-23 13:46:18 +0300

    Update Main.java

--- src/oopIntro/Main.java
+++ src/oopIntro/Main.java
@@ -6,8 +6,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		Product product1 = new Product(1, "Mercedes", "AMG335", 3000);
-		Product product2 = new Product(1, "BMW", "E250", 2500);
-		Product product3 = new Product(1, "Audi", "A7", 3000);
+		Product product2 = new Product(2, "BMW", "E250", 2500);
+		Product product3 = new Product(3, "Audi", "A7", 3000);
 
 		Product[] products = { product1, product2, product3 };
 
